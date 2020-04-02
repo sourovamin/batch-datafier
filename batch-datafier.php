0e947d0be455fa4359ac139ba36b75a026b00448
Sourov Amin <sourov.amin@gmail.com>
2020-04-02 18:53:55 +0200

    First published version

--- batch-datafier.php
+++ batch-datafier.php
@@ -4,24 +4,62 @@
  Plugin Name:User Batch Data Modifier 
  Description:Add/Update/Delete/Drop batch user data for all or specific users with meta key.
  Version: 1.0
- Author: Hossain Amin
+ Author: Sourov Amin
  License: GPLv2+
  Text Domain:batch-datafier
 */
 
+/*
+ * Add menu for the plugin under Users menu.
+ */
 add_action('admin_menu', 'bdm_add_menu');
 
 function bdm_add_menu() {
-  add_submenu_page( "users.php", "Batch Data Modifier", __("Batch Datafier", "batch-datafier"), "administrator", "bdm-main-menu", "bdm_main_menu"); 
+  add_submenu_page( "users.php", "Batch Data Modifier", __("Batch Modifier", "batch-datafier"), "administrator", "bdm-main-menu", "bdm_main_menu"); 
 }
 
+/*
+ * Dashboard for the plugin options.
+ */
 function bdm_main_menu(){
     if ( !is_admin() ){ return; }
+    
+    if (isset($_REQUEST["action"])){
+        if(current_user_can("edit_users")){
+            $nonce = $_REQUEST['_wpnonce'];
+            
+            if ($_REQUEST["action"] == "add"){
+                $metaKey = !empty($_POST["bdm_add_meta_key"]) ? sanitize_text_field($_POST["bdm_add_meta_key"]) : "";
+                $fieldData = !empty($_POST["bdm_add_field_data"]) ? sanitize_text_field($_POST["bdm_add_field_data"]) : "";
+                $roles = !empty($_POST["bdm_add_role"]) ? array_map("sanitize_text_field", $_POST["bdm_add_role"]) : [];
+                if(empty($metaKey) || empty($roles) || !wp_verify_nonce($nonce, "bdm_add_nonce")){
+                    echo "<div class='error notice'><p>".__("Operation Failed!", "batch-datafier")."</p></div>";
+                }else{
+                    bdm_insert_batch_data($metaKey, $fieldData, $roles);
+                }
+            }
+            
+            if ($_REQUEST["action"] == "delete"){
+                $metaKey = !empty($_POST["bdm_delete_meta_key"]) ? sanitize_text_field($_POST["bdm_delete_meta_key"]) : "";
+                $fieldData = !empty($_POST["bdm_delete_field_data"]) ? sanitize_text_field($_POST["bdm_delete_field_data"]) : "";
+                $roles = !empty($_POST["bdm_delete_role"]) ? array_map("sanitize_text_field", $_POST["bdm_delete_role"]) : [];
+                if(empty($metaKey) || empty($roles || !wp_verify_nonce($nonce, "bdm_delete_nonce"))){
+                    echo "<div class='error notice'><p>".__("Operation Failed!", "batch-datafier")."</p></div>";
+                }else{
+                    bdm_delete_batch_data($metaKey, $fieldData, $roles);
+                }
+            }
+        }
+        else{
+            die("Failed security check");
+        }
+    }
 ?>
     <div class="wrap">
         <p><?php echo __("* Following operations are irreversible. Please be sure before proceed!", "batch-datafier"); ?></p>
         <h1>Add/Update User Batch Data</h1>
-        <form id="bdm_add_data" action="" method="post">
+        <form id="bdm_add_data" action="<?php echo esc_url( admin_url( 'users.php?page=bdm-main-menu&action=add' ) ); ?>" method="post">
+            <?php wp_nonce_field('bdm_add_nonce'); ?>
             <table class="form-table">
                 <tr>
                     <th scope="row"><label for="bdm_add_meta_key"><?php echo __("Meta Key", "batch-datafier"); ?></label></th>
@@ -45,7 +83,8 @@ function bdm_main_menu(){
     <div class="wrap">
         <h1>Delete/Drop User Batch Data</h1>
         <p><?php echo __("* Leave 'User Data' field empty if you want to remove all metadata fields matching the meta key.", "batch-datafier"); ?></p>
-        <form id="bdm_delete_data" action="" method="post">
+        <form id="bdm_delete_data" action="<?php echo esc_url( admin_url( 'users.php?page=bdm-main-menu&action=delete' ) ); ?>" method="post">
+            <?php wp_nonce_field('bdm_delete_nonce'); ?>
             <table class="form-table">
                 <tr>
                     <th scope="row"><label for="bdm_delete_meta_key"><?php echo __("Meta Key", "batch-datafier"); ?></label></th>
@@ -66,29 +105,11 @@ function bdm_main_menu(){
         </form>
     </div>
 <?
-    if (isset($_POST["bdm_insert_data"])){
-        $metaKey = !empty($_POST["bdm_add_meta_key"]) ? sanitize_text_field($_POST["bdm_add_meta_key"]) : "";
-        $fieldData = !empty($_POST["bdm_add_field_data"]) ? sanitize_text_field($_POST["bdm_add_field_data"]) : "";
-        $roles = !empty($_POST["bdm_add_role"]) ? $_POST["bdm_add_role"] : [];
-        if(empty($metaKey) || empty($roles)){
-            echo "<div class='error notice'><p>".__("Operation Failed!", "batch-datafier")."</p></div>";
-        }else{
-            bdm_insert_batch_data($metaKey, $fieldData, $roles);
-        }
-    }
-    
-    if (isset($_POST["bdm_delete_data"])){
-        $metaKey = !empty($_POST["bdm_delete_meta_key"]) ? sanitize_text_field($_POST["bdm_delete_meta_key"]) : "";
-        $fieldData = !empty($_POST["bdm_delete_field_data"]) ? sanitize_text_field($_POST["bdm_delete_field_data"]) : "";
-        $roles = !empty($_POST["bdm_delete_role"]) ? $_POST["bdm_delete_role"] : [];
-        if(empty($metaKey) || empty($roles)){
-            echo "<div class='error notice'><p>".__("Operation Failed!", "batch-datafier")."</p></div>";
-        }else{
-            bdm_delete_batch_data($metaKey, $fieldData, $roles);
-        }
-    }
 }
 
+/*
+ * Insert or update data function.
+ */
 function bdm_insert_batch_data($metaKey, $fieldData, $roles){
     $count = 0;
     $query = new WP_User_Query(['role__in' => $roles]);
@@ -102,6 +123,9 @@ function bdm_insert_batch_data($metaKey, $fieldData, $roles){
     $metaKey."</strong> ".__("has been added/updated successfully for total users: ", "batch-datafier").$count."</div>";
 }
 
+/*
+ * Delete or drop data function.
+ */
 function bdm_delete_batch_data($metaKey, $fieldData, $roles){
     $count = 0;
     $query = new WP_User_Query(['role__in' => $roles]);
---
+++ readme.txt
@@ -0,0 +1,40 @@
+=== User Batch Data Modifier ===
+Contributors: sourov
+Tags:  usermeta, bulk, metakey, modify, user data, bulk add, batch delete, batch update
+Requires at least: 4.4
+Tested up to: 5.3.2
+Stable tag: 1.0
+Copyright: Hossain Amin (Sourov)
+License: GPLv2
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add/Update/Delete/Drop batch user data for all or specific users with meta key.
+
+== Description ==
+
+= Features =
+* Batch user data insert/update/delete/drop
+* Bulk user data processing with meta key
+* Role based data modification
+* Add new user data for all users or a group of users
+
+= Documentation =
+
+- Go to "Users >> Batch Modifier"
+- To add/update data put meta key, data and select your targeted users by roles. if the metakey already exists, it will update data for the selected users.
+- Batch data deletation is the same as above process. Leave "User Data" field empty if you want to remove all metadata fields matching the meta key.
+
+== Installation ==
+
+1. Upload and extract `batch-datafier.zip` to the `/wp-content/plugins/` directory or add the plugin from "Plugin >> Add New" menu
+2. Activate the plugin through the "Plugins" menu in WordPress
+
+== Screenshots ==
+
+1. Add/Update User Batch Data
+2. Delete User Batch Data
+
+== Changelog ==
+
+= 1.0.0 =
+* First version.
